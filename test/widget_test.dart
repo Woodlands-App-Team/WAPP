6fb4201543f351974bdce332256c918581570c65
Jeffrey-Kai Li <57923095+jeffreykaili@users.noreply.github.com>
2021-07-20 18:18:20 -0400

    moved folders

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:song_req_test/main.dart';
+import 'package:song_req_test/song_req_page/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
